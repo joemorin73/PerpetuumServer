ffe7b1aff9504341bea60ac861cb9a6257698830
mikejeffers <mike.s.jeffers@gmail.com>
2018-12-13 16:10:53 -0500

    targetpainters apply srf-area increase effect to target

--- src/Perpetuum/Modules/EffectModules/TargetPainterModule.cs
+++ src/Perpetuum/Modules/EffectModules/TargetPainterModule.cs
@@ -7,18 +7,22 @@ namespace Perpetuum.Modules.EffectModules
     public class TargetPainterModule : EffectModule
     {
         private readonly ItemProperty _effectStealthStrengthModifier;
+        private readonly ItemProperty _effectSurfaceAreaModifier;
 
         public TargetPainterModule() : base(true)
         {
             _effectStealthStrengthModifier = new ModuleProperty(this, AggregateField.effect_stealth_strength_modifier);
+            _effectSurfaceAreaModifier = new ModuleProperty(this, AggregateField.effect_signature_radius_modifier);
             AddProperty(_effectStealthStrengthModifier);
+            AddProperty(_effectSurfaceAreaModifier);
         }
 
         protected override void SetupEffect(EffectBuilder effectBuilder)
         {
             effectBuilder.SetType(EffectType.effect_target_painting)
                                 .SetSource(ParentRobot)
-                                .WithPropertyModifier(_effectStealthStrengthModifier.ToPropertyModifier());
+                                .WithPropertyModifier(_effectStealthStrengthModifier.ToPropertyModifier())
+                                .WithPropertyModifier(_effectSurfaceAreaModifier.ToPropertyModifier());
         }
     }
 }
