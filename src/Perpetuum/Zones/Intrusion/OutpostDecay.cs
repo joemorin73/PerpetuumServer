0f8df4d0948c5941e35f12e494721d8356a60a48
mikejeffers <mike.s.jeffers@gmail.com>
2019-09-08 19:17:42 -0400

    decay rates v1, docs

--- src/Perpetuum/Zones/Intrusion/OutpostDecay.cs
+++ src/Perpetuum/Zones/Intrusion/OutpostDecay.cs
@@ -5,17 +5,19 @@ using Perpetuum.EntityFramework;
 
 namespace Perpetuum.Zones.Intrusion
 {
-
+    /// <summary>
+    /// Outpost Decay handler - updates outpost stability with a system-generated SAP event
+    /// </summary>
     public class OutpostDecay
     {
         private readonly Outpost _outpost;
         private readonly EventListenerService _eventChannel;
-        private readonly static TimeSpan noDecayBefore = TimeSpan.FromDays(3);
+        private readonly static TimeSpan noDecayBefore = TimeSpan.FromDays(7);
         private readonly static TimeSpan decayRate = TimeSpan.FromDays(1);
         private TimeSpan timeSinceLastDecay = TimeSpan.Zero;
         private TimeSpan lastSuccessfulIntrusion = TimeSpan.Zero;
-        private readonly static int decayPts = -1;
-        private readonly EntityDefault def = EntityDefault.GetByName("def_outpost_decay"); //TODO DB PR
+        private readonly static int decayPts = -5;
+        private readonly EntityDefault def = EntityDefault.GetByName("def_outpost_decay");
 
         public OutpostDecay(EventListenerService eventChannel, Outpost outpost)
         {
@@ -32,12 +34,14 @@ namespace Perpetuum.Zones.Intrusion
             timeSinceLastDecay += time;
             if (timeSinceLastDecay > decayRate)
             {
-                Console.WriteLine("Outpost is decaying!");
                 timeSinceLastDecay = TimeSpan.Zero;
                 DoDecay();
             }
         }
 
+        /// <summary>
+        /// Called when a SAP is completed
+        /// </summary>
         public void OnSAP()
         {
             lastSuccessfulIntrusion = TimeSpan.Zero;
